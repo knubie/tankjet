ec49574711848e52dd5c27d47fd02098d33aa0f9
Matt <matt@tankjetstudios.com>
2012-01-17 20:32:15 -0500

    did some stuff

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 (function ($) {
-	$(document).ready(function () {
+	$(document).waitForImages(function () {
 		var win = $(window),
 				sc = $(".slides-container");
 		function init() {
@@ -8,15 +8,13 @@
 				$(".resize-me").centerCropFit(win.width(), win.height())
 				$(".slides-container, .img-container").css("width", $(window).width());
 				$(".slides-container, .img-container").css("height", $(window).height());
-				function centerImages() {
-					$(".center-me").css('margin-top', function() {
-						return (win.height() / 3) - ($(this).height() / 2);
-					});
-					$(".logo").css('margin-left', function() {
-						return (win.width() / 2) - ($(this).width() / 2);
-					});
-				};
-				centerImages();
+				$(".logo").fit(win.width()/3.3, win.height()/3.3, (932/409))
+				$(".center-me").css('margin-top', function() {
+					return (win.height() / 3) - ($(this).height() / 2);
+				});
+				$(".logo").css('margin-left', function() {
+					return (win.width() / 2) - ($(this).width() / 2);
+				});
 			};
 			resize();
 			win.resize(resize);
@@ -24,7 +22,7 @@
 			$(".slides-container").cycle({
 				fx: 'scrollLeft',
 				fit: 1,
-				slideResize: 1
+				slideResize: 0
 			});
 			
 		};
