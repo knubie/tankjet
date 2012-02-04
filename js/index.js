7926f0db846c6c6675ef7395bc358b5493a0451e
Matthew Steedman <matt@tankjetstudios.com>
2012-02-04 11:05:38 -0500

    removed logo from splash

--- js/index.js
+++ js/index.js
@@ -8,12 +8,8 @@
 				$(".resize-me").centerCropFit(win.width(), win.height())
 				$(".slides-container, .img-container").css("width", $(window).width());
 				$(".slides-container, .img-container").css("height", $(window).height());
-				$(".logo").fit(win.width()/3.3, win.height()/3.3, (932/409))
 				$(".center-me").css('margin-top', function() {
 					return (win.height() / 3) - ($(this).height() / 2);
-				});
-				$(".logo").css('margin-left', function() {
-					return (win.width() / 2) - ($(this).width() / 2);
 				});
 			};
 			resize();
